d5a16edd5397660f686863fa155bc7eaff936ba7
Davi Viana <davi.vc@hotmail.com>
2020-11-27 16:34:44 -0300

    update

---
+++ java/Interface.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Interface {
+    private RegrasJogo jogo = new RegrasJogo();
+    private Scanner sc = new Scanner(System.in);
+    public void iniciar(){
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        desenharTabuleiro();
+        System.out.println("\ndigite a cordenada da peca (y,x)\n Ex: 61");
+        System.out.print("> ");
+        String p = sc.next();
+        System.out.println("digite o movimento vertical \n Ex: -1");
+        System.out.print("> ");
+        int ym = sc.nextInt();
+        System.out.println("digite o movimento horizontal\n Ex: 0");
+        System.out.print("> ");
+        int xm = sc.nextInt();
+        int[] peca = {Integer.parseInt(p.substring(0,1)),Integer.parseInt(p.substring(1,2))};
+        int[] movimento = {ym,xm};
+        jogo.movimentarPeca(peca, movimento);
+
+        iniciar(); // a interface não vai ser recursiva, é so um teste
+    }
+    public void desenharTabuleiro(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j< 8; j++){
+                if(j==0)
+                    System.out.print(i + "  ");
+                System.out.print(jogo.getTabuleiro()[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n   0 1 2 3 4 5 6 7");
+    }
+}
---
+++ java/RegrasJogo.java
@@ -0,0 +1,54 @@
+public class RegrasJogo {
+
+    private char[][] tabuleiro = {{'T', 'C', 'B', 'A', 'R', 'B', 'C', 'T'},
+                                  {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                                  {' ', ' ', ' ', ' ',' ', ' ', ' ', ' '},
+                                  {' ', ' ', ' ', ' ',' ', ' ', ' ', ' '},
+                                  {' ', ' ', ' ', ' ',' ', ' ', ' ', ' '},
+                                  {' ', ' ', ' ', ' ',' ', ' ', ' ', ' '},
+                                  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                                  {'t', 'c', 'b', 'a', 'r', 'b', 'c', 't'}};
+
+    public char[][] getTabuleiro(){
+        return tabuleiro;
+    }
+
+    public int movimentarPeca(int[] peca, int[] movimento){
+        if(tabuleiro[peca[0]][peca[1]] == 'p') {
+            int[] resultante = {peca[0] + movimento[0], peca[1] + movimento[1]};
+
+            if(resultante[0] > 7 || resultante[0] < 0 || resultante[1] > 7 || resultante[1] < 0 ) // fora dos limites
+                return -1;
+            if(resultante[0] > peca[0]) // andou para tras
+                return -1;
+            if(movimento[0] == 0) // não andou verticalmente
+                return -1;
+            if(resultante[1] != peca[1] && tabuleiro[resultante[0]][resultante[1]] == ' ') // andou para o lado sem peca para matar
+                return -1;
+            if(movimento[1] > 1 || movimento[1] < -1) // andou mais que uma casa para o lado
+                return -1;
+            if(movimento[0] == -2 && peca[0] != 6) // andou duas casas para frente após a primeira jogada
+                return -1;
+            if(movimento[0] == -2 && movimento[1] != 0) // andou duas casas para frente e tambem uma para o lado
+                return -1;
+            if(movimento[0] < -2) // andou mais que duas casas para frente
+                return -1;
+            if(movimento[1] == 0 && tabuleiro[resultante[0]][resultante[1]] != ' ') // andou apenas para frente caindo numa casa ocupada
+                return -1;
+            if(movimento[1] == 0 && movimento[0] == -2 && tabuleiro[resultante[0]+1][resultante[1]] != ' ') // andou duas casas para frente com a casa intermediaria ocupada
+                return -1;
+
+            tabuleiro[resultante[0]][resultante[1]] = 'p';
+            tabuleiro[peca[0]][peca[1]] = ' ';
+            return 0;
+        }else if(tabuleiro[peca[0]][peca[1]] == 'P') {
+            
+        }
+        return -1;
+    }
+
+
+
+
+
+}
